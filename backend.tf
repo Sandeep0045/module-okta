8d06084b21dde2461e926f32748640f2cd17fab9
Sandeep0045 <38896678+Sandeep0045@users.noreply.github.com>
2020-09-17 23:49:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "okta-statefiles"
     key    = "group.tfstate"
     region = "us-west-2"
-    access_key = "AKIAYHUP6XMKLCIX2MHN"
-    secret_key = "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"
+    access_key = 
+    secret_key = 
   }
 }
