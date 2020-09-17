f5f54e43f4dd3bb2d0cf965b56b8ef7bca61076d
Sandeep <you@example.com>
2020-09-17 15:23:04 +0530

    11

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "group"
-    key    = "remotedemo.tfstate"
+    bucket = "okta-statefiles"
+    key    = "group.tfstate"
     region = "us-west-2"
     access_key = "AKIAYHUP6XMKLCIX2MHN"
     secret_key = "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"
--- modules/main/vars.tf
+++ modules/main/vars.tf
@@ -1,24 +1,24 @@
 variable "first_name" {
-  default = "group1"
+  default = "group12"
 }
 
 variable "last_name" {
-  default = "group1"
+  default = "group12"
 }
 
 variable "login" {
-  default = "group1@gmail.com"
+  default = "group12@gmail.com"
 }
 
 variable "email" {
-  default = "group1@gmail.com"
+  default = "group12@gmail.com"
 }
  
 variable "name" {
-  default = "group1"
+  default = "group12"
 }
 
 variable "description" {
-  default = " group1"
+  default = " group12"
 
 }
